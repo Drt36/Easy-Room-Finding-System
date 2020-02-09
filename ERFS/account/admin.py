6508e20340aaa3d9c5bec5136aa1ad882250f3fd
Drt36 <58885633+Drt36@users.noreply.github.com>
2020-02-09 16:18:10 +0545

    update

--- ERFS/account/admin.py
+++ ERFS/account/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import UserProfile
+from booking.models import Asset,Booking
 admin.site.register(UserProfile)
+admin.site.register(Asset)
+admin.site.register(Booking)
