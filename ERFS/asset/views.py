b79ca59aa481dc907ac1ce5fb0a30f24847b1f02
ajaltdr <58896248+ajaltdr@users.noreply.github.com>
2020-02-09 14:24:48 +0545

    Adding views for search

--- ERFS/asset/views.py
+++ ERFS/asset/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from booking.models import Asset
+from django.contrib import messages
+from django.db.models import Q
+from django import template
+
+
 
 # Create your views here.
+
+
+
+def result(request):
+    query =" "
+    context = {}
+    if request.GET:
+        query = request.GET['q']
+    asset = get_data_queryset(query)
+    context['asset'] = asset
+    
+    return render(request, "result.html", context)  
+
+def get_data_queryset(query=None):
+    queryset = []
+    queries = query.split(" ")
+    for q in queries:
+        assets = Asset.objects.filter(Q(asset_Title__startswith=q) | Q(asset_Title__icontains=q))
+
+        for asset in assets:
+            queryset.append(asset)
+    return list(set(queryset))            
+
+
+
